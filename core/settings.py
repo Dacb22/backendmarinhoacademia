d7bee9265e0d0b4eca9f83ff70309d99dd59765c
driouxnolol <danilo.cavalcante@live.com>
2024-06-23 11:22:54 -0300

    Ajustando para deploy

--- core/settings.py
+++ core/settings.py
@@ -157,6 +157,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(days=1)
 }
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
